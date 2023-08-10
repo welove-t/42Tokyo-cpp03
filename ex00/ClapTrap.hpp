4a1352fa1f2fc683bf21e1bb027791eb06359578
love <tlove.tky@gmail.com>
2023-08-10 15:33:40 +0900

    ex00: first commit

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+#define RED		"\e[0;31m" // Red
+#define GREEN	"\e[0;32m" // Green
+#define YELLOW	"\e[0;33m" // Yellow
+#define BLUE	"\e[0;34m" // Blue
+#define RESET 	"\e[0m"    // Reset
+
+class ClapTrap
+{
+	private:
+
+	public:
+
+};
+
+#endif
