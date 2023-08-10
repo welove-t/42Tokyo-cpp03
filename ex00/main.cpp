e86e8be6af87fdd27800ef5cf6a0e527731aa978
love <tlove.tky@gmail.com>
2023-08-10 17:10:32 +0900

    ex00: 初回テストの実装

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,6 +2,28 @@
 
 int main( void )
 {
+	std::cout << BLUE <<
+		"------------------------------------------" << std::endl <<
+		"               ClapTrap !" 				<< std::endl <<
+		"            pikachu vs pichu)" 			<< std::endl <<
+		"------------------------------------------" << std::endl <<
+	RESET << std::endl;
+
+	ClapTrap pikachu("pikachu");
+	ClapTrap pichu("pichu");
+
+	pikachu.printStatus();
+	pichu.printStatus();
+	pikachu.setAttackPoint(8);
+	pichu.setAttackPoint(4);
+
+	std::cout << BLUE <<
+		"     pikachu -> pichu     " 				<< std::endl <<
+	RESET << std::endl;
+	pikachu.attack("pichu");
+	pichu.takeDamage(pikachu.getAttackPoint());
+	pikachu.printStatus();
+	pichu.printStatus();
 
 	return 0;
 }
